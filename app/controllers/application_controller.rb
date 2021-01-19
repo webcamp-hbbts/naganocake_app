0d143ba482c3b13d1c0e9d4b4307b6e789b84920
KengoMatsuo0529 <k.matsuo0529@gmail.com>
2021-01-19 09:46:19 +0000

    after_signin_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
     
+    def after_sign_in_path_for(resource)
+        customer_customers_path # ログイン後に遷移するpathを設定
+    end
     
+    def after_sign_out_path_for(resource)
+        root_path # ログアウト後に遷移するpathを設定
+    end
     
     protected
     
