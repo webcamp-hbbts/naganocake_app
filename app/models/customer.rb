bb6d106533fb6989cbc69ffdbc5faa3dd623730b
martinbicycle <martinbicycle7700@gmail.com>
2021-01-20 10:03:15 +0000

    [カスタマービュー完成]

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -20,5 +20,8 @@ class Customer < ApplicationRecord
     end
     
     validates :is_deleted, inclusion: {in: [true, false]}
+    def active_for_authentication?
+        super && (self.is_deleted == false)
+    end
 
 end
