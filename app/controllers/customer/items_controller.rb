90382f28e28316c47d031d03ea6b44ad4682a4f2
EC2 Default User <s12201600350@gmail.com>
2021-01-20 10:23:59 +0000

    cart_item_views

--- app/controllers/customer/items_controller.rb
+++ app/controllers/customer/items_controller.rb
@@ -1,10 +1,12 @@
 class Customer::ItemsController < ApplicationController
 
+
     def index
-        @items = Item.page(params[:page]).per(8)
+     @items = Item.all
     end
-    
+
     def show
-        @item = Item.find(params[:id])
+     @item = Item.find(params[:id])
+     @cart_item = CartItem.new
     end
 end
