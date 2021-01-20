86713073bdbc59af0cb504e30228c93dc4d4064f
KengoMatsuo0529 <k.matsuo0529@gmail.com>
2021-01-20 02:59:06 +0000

    fix-customer-view

--- app/controllers/customer/homes_controller.rb
+++ app/controllers/customer/homes_controller.rb
@@ -1,7 +1,7 @@
 class Customer::HomesController < ApplicationController
 
     def top
-        @items = Item.all
+        @items = Item.limit(4).order(" created_at DESC ")
     end
     
     def about
