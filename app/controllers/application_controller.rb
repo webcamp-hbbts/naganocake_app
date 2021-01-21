cd3a3662e0256547e2d2a2a32d4d1df4e07aeacf
KengoMatsuo0529 <k.matsuo0529@gmail.com>
2021-01-21 05:39:07 +0000

    view-fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,34 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
-    
+
     def after_sign_in_path_for(resource)
-        customer_customers_path # ログイン後に遷移するpathを設定
+        case resource
+          when Admin
+            admin_orders_path
+          when Customer
+            customer_customers_path
+        end
     end
-    
+
     def after_sign_out_path_for(resource)
-        root_path # ログアウト後に遷移するpathを設定
+        case resource
+          when Admin
+            new_admin_session_path
+          when Customer
+            root_path
+        end
     end
-    
+
+      # def after_sign_out_path_for(admin)
+      #   new_admin_session_path
+      # end
+
+      # def after_sign_in_path_for(resource)
+      #   root_path
+      # end
+
     protected
-    
+
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up,keys: [
      :last_name,
@@ -20,6 +38,6 @@ class ApplicationController < ActionController::Base
      :postcode,
      :address,
      :phone_number,
-   ])  
+   ])
     end
 end
