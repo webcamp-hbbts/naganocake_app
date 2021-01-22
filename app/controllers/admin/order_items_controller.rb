44d62191a8c2e9af7f6d0b0a6f34d3a1b4d466ab
EC2 Default User <s12201600350@gmail.com>
2021-01-22 07:20:44 +0000

    fixed name

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -7,10 +7,10 @@ class Admin::OrderItemsController < ApplicationController
     @order_item = OrderItem.find(params[:id])
     @order_item.update(order_item_params)
     @order = @order_item.order
-    if @order.order_items.where(production_status: "制作完了").to_a.count == @order.order_items.to_a.count
+    if @order.order_items.where(production_status: "製作完了").to_a.count == @order.order_items.to_a.count
       @order.update(status: "発送準備中")
-    else @order.order_items.where(production_status: "制作中").to_a.count != 0
-      @order.update(status: "制作中")
+    else @order.order_items.where(production_status: "製作中").to_a.count != 0
+      @order.update(status: "製作中")
     end
     redirect_to admin_order_path(@order)
   end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,9 +2,9 @@ class OrderItem < ApplicationRecord
 
     enum production_status: {
         着手不可: 0,
-        制作待ち: 1,
-        制作中: 2,
-        制作完了: 3
+        製作待ち: 1,
+        製作中: 2,
+        製作完了: 3
     }
 
     belongs_to :item
